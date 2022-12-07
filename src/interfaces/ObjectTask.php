5bbf017865e20b907dec08a5c46dfe13336fe6c2
Vadim <project.k.vadim@gmail.com>
2022-12-07 12:12:51 +0200

    added methods

--- src/interfaces/ObjectTask.php
+++ src/interfaces/ObjectTask.php
@@ -13,4 +13,37 @@ namespace vadimcontenthunter\GitScripts\interfaces;
  */
 interface ObjectTask
 {
+    /**
+     * Метод устанавливает уникальный index для задачи, на основе существующих.
+     *
+     * @param array<int|string> $index Массив существующих индексов.
+     *
+     * @return mixed
+     */
+    public function setIndex(array $index): mixed;
+
+    /**
+     * Метод устанавливает наименование для задачи.
+     *
+     * @param string $title
+     *
+     * @return mixed
+     */
+    public function setTitle(string $title): mixed;
+
+    /**
+     * Метод устанавливает путь к выполняемому фалу текущей задачи
+     *
+     * @param string $path
+     *
+     * @return mixed
+     */
+    public function setExecutionPath(string $path): mixed;
+
+    /**
+     * Метод выполняет текущую задачу.
+     *
+     * @return bool Возвращает в случае успеха true, иначе false.
+     */
+    public function execute(): bool;
 }
