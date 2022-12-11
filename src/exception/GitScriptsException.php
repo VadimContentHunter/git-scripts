84ef270d54b86b443bf303dd8520c9287c0edfae
Vadim <project.k.vadim@gmail.com>
2022-12-11 14:41:03 +0200

    added new main exception

---
+++ src/exception/GitScriptsException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vadimcontenthunter\GitScripts\exception;
+
+/**
+ * Класс с исключениями для Git-Scripts
+ *
+ * @package   GitScripts_Exception
+ * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
+ * @copyright (c) Vadim Volkovskyi 2022
+ */
+class GitScriptsException extends \Exception
+{
+}
