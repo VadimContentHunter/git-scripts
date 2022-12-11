ca7f2f4b8c36ba23924758e9fb37c52766a63d13
Vadim <project.k.vadim@gmail.com>
2022-12-11 16:21:57 +0200

    added implementation of the Tasks class

--- src/Tasks.php
+++ src/Tasks.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace vadimcontenthunter\GitScripts;
 
+use vadimcontenthunter\GitScripts\exception\GitScriptsException;
+use vadimcontenthunter\GitScripts\interfaces\ObjectTask;
+
 /**
- * Класс запускает задачи.
+ * Класс работает с списком задач.
  *
  * @package   GitScripts
  * @author    Vadim Volkovskyi <project.k.vadim@gmail.com>
@@ -13,4 +16,46 @@ namespace vadimcontenthunter\GitScripts;
  */
 class Tasks
 {
+    /**
+     * Параметр хранит индекс задачи
+     *
+     * @var array<ObjectTask>
+     */
+    protected array $taskList = [];
+
+    /**
+     * Метод добавляет задачу в список на выполнение
+     *
+     * @param ObjectTask $objectTask
+     *
+     * @return Tasks
+     */
+    public function addTaskList(ObjectTask $objectTask): Tasks
+    {
+        return $this;
+    }
+
+    /**
+     * Метод удаляет задачу по указанному индексу из писка на выполнения
+     *
+     * @param string $index
+     *
+     * @return bool Возвращает в случае успешного удаления true, иначе false.
+     */
+    public function deleteTaskList(string $index): bool
+    {
+        return false;
+    }
+
+    /**
+     * Метод запускает список задач на выполнение.
+     * Добавить или удалить задачи из списка, после этого запуска нельзя.
+     *
+     * @return void
+     *
+     * @throws GitScriptsException
+     */
+    public function start(): void
+    {
+    }
 }
