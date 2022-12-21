30f77a8b64fd7775054dbb9e94446e8a6353ca8a
Vadim <project.k.vadim@gmail.com>
2022-12-21 10:17:25 +0200

    added new test for getTaskList method

--- tests/TasksTest.php
+++ tests/TasksTest.php
@@ -6,10 +6,11 @@ namespace vadimcontenthunter\GitScripts\Tests;
 
 use Psr\Log\NullLogger;
 use PHPUnit\Framework\TestCase;
-use vadimcontenthunter\GitScripts\TaskProgressLevel;
 use vadimcontenthunter\GitScripts\Tasks;
+use vadimcontenthunter\GitScripts\TaskProgressLevel;
 use vadimcontenthunter\GitScripts\interfaces\ObjectTask;
 use vadimcontenthunter\GitScripts\Tests\src\fakes\TasksFake;
+use vadimcontenthunter\GitScripts\exception\GitScriptsException;
 use vadimcontenthunter\GitScripts\Tests\src\fakes\ObjectTaskFake;
 use vadimcontenthunter\GitScripts\Tests\src\fakes\StandardTaskFake;
 
@@ -106,8 +107,9 @@ class TasksTest extends TestCase
      * @test
      * @dataProvider providerDeleteTaskList
      *
-     * @param array<ObjectTask> $taskList           Список задач который будет добавлен
-     * @param array<ObjectTask> $expectableResult   Ожидаемый результат
+     * @param array<ObjectTask> $taskList Список задач который будет добавлен
+     * @param string $index Индекс задачи для удаления
+     * @param array<ObjectTask> $expectableResult Ожидаемый результат
      *
      * @return void
      */
@@ -154,6 +156,114 @@ class TasksTest extends TestCase
                         ->fakeSetParameterExecutionPath($path)
                         ->fakeSetParameterExecutionStatus(TaskProgressLevel::WAITING),
                 ],
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider providerGetTaskList
+     *
+     * @param array<ObjectTask> $taskList Список задач который будет добавлен
+     * @param array<ObjectTask> $expectableResult Ожидаемый результат
+     *
+     * @return void
+     */
+    public function test_getTaskList_withoutParameters_shouldReturnTaskList(
+        array $taskList,
+        array $expectableResult
+    ): void {
+        $this->tasksFake->fakeSetTaskList($taskList);
+        $taskList = $this->tasksFake->getTaskList();
+
+        $this->assertEquals($expectableResult, $taskList);
+    }
+
+    public function providerGetTaskList(): array
+    {
+        $path = '.\\test\\test.test';
+        return [
+            'Test 1' => [
+                [
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('1')
+                        ->fakeSetParameterTitle('Task 1')
+                        ->fakeSetParameterExecutionPath($path),
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('2')
+                        ->fakeSetParameterTitle('Task 2')
+                        ->fakeSetParameterExecutionPath($path),
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('3')
+                        ->fakeSetParameterTitle('Task 3')
+                        ->fakeSetParameterExecutionPath($path),
+                ],
+                [
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('1')
+                        ->fakeSetParameterTitle('Task 1')
+                        ->fakeSetParameterExecutionPath($path)
+                        ->fakeSetParameterExecutionStatus(TaskProgressLevel::WAITING),
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('2')
+                        ->fakeSetParameterTitle('Task 2')
+                        ->fakeSetParameterExecutionPath($path)
+                        ->fakeSetParameterExecutionStatus(TaskProgressLevel::WAITING),
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('3')
+                        ->fakeSetParameterTitle('Task 3')
+                        ->fakeSetParameterExecutionPath($path)
+                        ->fakeSetParameterExecutionStatus(TaskProgressLevel::WAITING),
+                ],
+            ],
+        ];
+    }
+
+     /**
+     * @test
+     * @dataProvider providerGetTaskListExceptions
+     *
+     * @param string $executionPath Параметр для метода
+     * @param \Exception $expectableResult Ожидаемый результат
+     */
+    public function test_getTaskList_withoutParameters_shouldThrowAnException(
+        array $taskList,
+        \Exception $expectableResult
+    ): void {
+        $this->expectException($expectableResult::class);
+
+        $this->tasksFake->getTaskList();
+    }
+
+    public function providerGetTaskListExceptions(): array
+    {
+        $path = '.\\test\\test.test';
+        return [
+            'Test 1' => [
+                [
+                    'test'
+                ],
+                new GitScriptsException(),
+            ],
+            'Test 2' => [
+                [
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('1')
+                        ->fakeSetParameterTitle('Task 1')
+                        ->fakeSetParameterExecutionPath($path),
+                    'test'
+                ],
+                new GitScriptsException(),
+            ],
+            'Test 3' => [
+                [
+                    (new StandardTaskFake(new NullLogger()))
+                        ->fakeSetParameterIndex('1')
+                        ->fakeSetParameterTitle('Task 1')
+                        ->fakeSetParameterExecutionPath($path),
+                    4442
+                ],
+                new GitScriptsException(),
             ],
         ];
     }
